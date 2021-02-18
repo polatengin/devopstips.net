f68a01ac1e2918f395a754f94b79c4dc1a19ecb6
polatengin <enpolat@microsoft.com>
2021-02-19 00:13:20 +0300

    common post item element used in the site

---
+++ src/components/post-item.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+export default ({ post }) => {
+  return (
+    <article className="my-6 text-gray-700 dark:text-gray-100" itemScope itemType="http://schema.org/Article">
+      <header className="mb-3">
+        <Link to={post.fields.slug} className="hover:underline text-3xl">{ post.frontmatter.title }</Link>
+        <p className="mt-3 text-xs text-gray-500 flex items-center">
+          by
+          <img className="inline rounded-full bg-gray-500 mx-1 w-4 h-4" src={post.frontmatter.author.profilePictureUri} />
+          <Link to={`/author/${post.frontmatter.author.alias}`} className="mr-1 text-blue-500">{ post.frontmatter.author.name }</Link>
+          @ [<em>{post.frontmatter.date}</em>]
+          <span className="mx-1">~ { post.frontmatter.time } read</span>
+          · under
+          { post.frontmatter.category.map((category, index) => {
+            return <a key={index} className={`text-xs p-1 ml-1 category-${category}`} href="#">{ category }</a>;
+          }) }
+        </p>
+      </header>
+      <section>
+        <p dangerouslySetInnerHTML={{ __html: post.frontmatter.description }} itemProp="description" />
+      </section>
+      <Link to={post.fields.slug} className="text-blue-500 text-xs">Read more →</Link>
+    </article>
+  );
+};
