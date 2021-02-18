be856b2e9c55a463d25f8dc7d148e973dcc98005
polatengin <enpolat@microsoft.com>
2021-02-19 00:14:24 +0300

    all posts page created

---
+++ src/pages/posts.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import PostItem from "../components/post-item"
+
+export default ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Posts" />
+      <h1 className="text-lg text-gray-600 dark:text-gray-100 mb-3 border-b">All Posts</h1>
+      <ol style={{ listStyle: `none` }}>
+        {posts.map((post, index) => {
+          return (
+            <li key={post.fields.slug} className={index > 0 ? "border-t" : ""}>
+              <PostItem post={post} />
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          time
+          title
+          author {
+            name
+            alias
+            profilePictureUri
+          }
+          description
+          category
+        }
+      }
+    }
+  }
+`
