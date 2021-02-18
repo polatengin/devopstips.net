3ca04e3786cb440feb9142746e64fad2daa6b56b
polatengin <enpolat@microsoft.com>
2021-02-19 00:07:05 +0300

    gatsby browser config created

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+// custom typefaces
+import "typeface-montserrat"
+import "typeface-merriweather"
+// custom CSS styles
+import "./src/bundle.css"
+
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
