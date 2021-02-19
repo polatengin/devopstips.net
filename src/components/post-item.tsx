3f6619b9dfe74a2bd31b004c4e1a729aa79028d0
polatengin <enpolat@microsoft.com>
2021-02-19 14:35:25 +0300

    path field from frontmatter used instead of slug

--- src/components/post-item.tsx
+++ src/components/post-item.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 
-import { Link, graphql } from "gatsby"
+import { Link } from "gatsby"
 
 export default ({ post }) => {
   return (
     <article className="my-6 text-gray-700 dark:text-gray-100" itemScope itemType="http://schema.org/Article">
       <header className="mb-3">
-        <Link to={post.fields.slug} className="hover:underline text-3xl">{ post.frontmatter.title }</Link>
+        <Link to={ `/${ post.frontmatter.path }` } className="hover:underline text-3xl">{ post.frontmatter.title }</Link>
         <p className="mt-3 text-xs text-gray-500 flex items-center">
           by
           <img className="inline rounded-full bg-gray-500 mx-1 w-4 h-4" src={post.frontmatter.author.profilePictureUri} />
@@ -15,14 +15,14 @@ export default ({ post }) => {
           <span className="mx-1">~ { post.frontmatter.time } read</span>
           · under
           { post.frontmatter.category.map((category, index) => {
-            return <a key={index} className={`text-xs p-1 ml-1 category-${category}`} href="#">{ category }</a>;
+            return <a key={ index } className={`text-xs p-1 ml-1 category-${ category }`} href="#">{ category }</a>;
           }) }
         </p>
       </header>
       <section>
         <p dangerouslySetInnerHTML={{ __html: post.frontmatter.description }} itemProp="description" />
       </section>
-      <Link to={post.fields.slug} className="text-blue-500 text-xs">Read more →</Link>
+      <Link to={ `/${ post.frontmatter.path }` } className="text-blue-500 text-xs">Read more →</Link>
     </article>
   );
 };
