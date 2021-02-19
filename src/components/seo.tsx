ed7d010d9cfb39c566cb9751af852d5b54564e2e
polatengin <enpolat@microsoft.com>
2021-02-19 09:40:13 +0300

    document formatted

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,7 +1,10 @@
 import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
+
 import { useStaticQuery, graphql } from "gatsby"
+
+import PropTypes from "prop-types"
+
+import { Helmet } from "react-helmet"
 
 const SEO = ({ description, lang, meta, title }) => {
   const { site } = useStaticQuery(
