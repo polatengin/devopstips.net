ee129ac255dfd0ee0ffec15ce11c0d0bc794b36b
polatengin <enpolat@microsoft.com>
2021-02-20 16:05:46 +0300

    rss svg icon added

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -102,6 +102,14 @@ export const TwitterIcon: FunctionComponent<IconProps> = (props: IconProps) => {
   );
 };
 
+export const RSSIcon: FunctionComponent<IconProps> = (props: IconProps) => {
+  return (
+    <svg fill="none" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" className={props.className}>
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
+    </svg>
+  );
+};
+
 export const GitHubIcon: FunctionComponent<IconProps> = (props: IconProps) => {
   return (
     <svg fill="currentColor" width="24" height="24" viewBox="0 0 256 250" stroke="currentColor" className={props.className}>
