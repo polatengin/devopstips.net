ff935f03b44bed8da16ad6c73ce5faf35d07bf97
polatengin <enpolat@microsoft.com>
2021-06-23 15:27:46 +0300

    comments removed

--- gatsby-node.js
+++ gatsby-node.js
@@ -86,12 +86,6 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
 
-  // Explicitly define the siteMetadata {} object
-  // This way those will always be defined even if removed from gatsby-config.js
-
-  // Also explicitly define the Markdown frontmatter
-  // This way the "MarkdownRemark" queries will return `null` even when no
-  // blog posts are stored inside "content/blog" instead of returning an error
   createTypes(`
     type SiteSiteMetadata {
       author: Author
